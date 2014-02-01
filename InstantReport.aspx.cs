f5af17f9814dc84c772ff130b1cf1f8742994832
Scott McLeod <halcyonblue@gmail.com>
2014-01-31 22:55:12 -0500

    Removing BOM from InstantReport.aspx.cs

--- InstantReport.aspx.cs
+++ InstantReport.aspx.cs
@@ -1,6 +1,5 @@
-﻿public partial class InstantReport : System.Web.UI.Page {
+public partial class InstantReport : System.Web.UI.Page {
   protected override void OnPreInit(System.EventArgs e) {
     ASP.global_asax.CustomAdHocConfig.InitializeReporting();
   }
 }
-
