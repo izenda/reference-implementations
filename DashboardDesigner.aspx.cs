5dad9b852c6978da5bfb762b37a4abdcd53b2e42
Scott McLeod <halcyonblue@gmail.com>
2014-02-06 18:41:23 -0500

    Removing cs versions of DashboardDesigner and DashboardViewer

--- DashboardDesigner.aspx.cs
+++
@@ -1,5 +0,0 @@
-﻿public partial class DashboardDesigner : System.Web.UI.Page {
-  protected override void OnPreInit(System.EventArgs e) {
-    ASP.global_asax.CustomAdHocConfig.InitializeReporting();
-  }
-}
--- DashboardViewer.aspx.cs
+++
@@ -1,5 +0,0 @@
-public partial class DashboardViewer : System.Web.UI.Page {
-  protected override void OnPreInit(System.EventArgs e) {
-    ASP.global_asax.CustomAdHocConfig.InitializeReporting();
-  }
-}
