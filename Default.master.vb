475e929d4c8d59775d39b2558e042b6be971706b
Scott McLeod <halcyonblue@gmail.com>
2014-02-06 19:45:20 -0500

    Adding Default.master.vb

---
+++ Default.master.vb
@@ -0,0 +1,22 @@
+Imports Izenda.AdHoc
+
+Partial Class MasterPage1
+  Inherits MasterPage
+  Protected Overrides Sub OnInit(ByVal e As System.EventArgs)
+    ASP.global_asax.CustomAdHocConfig.InitializeReporting()
+    If (Not String.IsNullOrEmpty(AdHocSettings.ApplicationHeaderImageUrl)) Then
+      rightLogo.Src = AdHocSettings.ApplicationHeaderImageUrl
+    End If
+    If (AdHocSettings.ShowDesignLinks = False) Then
+      Dim script As String
+      script = "<script type=""text/javascript"" language=""javascript"">"
+      script += "try { $(document).ready(function() {$('.designer-only').hide(); });}catch(e){}"
+      script += " try{ jq$(document).ready(function() {jq$('.designer-only').hide(); });}catch(e){} "
+      script += "</script>"
+      If Not (Page Is Nothing) Then
+        Page.Header.Controls.Add(New LiteralControl(script))
+      End If
+    End If
+    AdHocSettings.ShowSettingsButtonForNonAdmins = False
+  End Sub
+End Class
