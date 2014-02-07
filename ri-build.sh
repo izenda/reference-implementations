55a99147e8425dce1484c5dc16dc7ea623e229b4
Scott McLeod <smcleod@smcleod-ubuntu.(none)>
2014-02-07 12:58:40 -0500

    Updating ri-build.sh

--- ri-build.sh
+++ ri-build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+RI=${1}
 wget http://archives.izenda.us/core/latest/izenda.adhoc.zip
 unzip izenda.adhoc.zip
 rm izenda.adhoc.zip
 mkdir -p Reports
 touch Izenda.config
-mkdir -p webforms-cs
-find . ! -name "webforms-cs" | cpio -mpvd webforms-cs
-zip -r webforms-cs.zip webforms-cs
+mkdir -p "${RI}"
+find . ! -name "${RI}" | cpio -mpvd "${RI}"
+zip -r ${RI}.zip ${RI}
