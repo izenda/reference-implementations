dd1fb2ec564a9517553c2198b55e0583a905dbfc
Scott McLeod <halcyonblue@gmail.com>
2014-02-06 20:17:55 -0500

    Adding VB ReportList

--- ReportList.aspx.cs
+++
@@ -1,5 +0,0 @@
-﻿public partial class ReportList : System.Web.UI.Page {
-  protected override void OnPreInit(System.EventArgs e) {
-    ASP.global_asax.CustomAdHocConfig.InitializeReporting();
-  }
-}
