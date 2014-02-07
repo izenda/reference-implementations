1e40c9c6104035a3dea1b3741e4e376c2050021c
Scott McLeod <halcyonblue@gmail.com>
2014-02-06 20:29:37 -0500

    Adding VB Settings

---
+++ Settings.aspx.vb
@@ -0,0 +1,6 @@
+Partial Class Settings
+  Inherits System.Web.UI.Page
+  Protected Overrides Sub OnPreInit(ByVal e As System.EventArgs)
+    ASP.global_asax.CustomAdHocConfig.InitializeReporting()
+  End Sub
+End Class
