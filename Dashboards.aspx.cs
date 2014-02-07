dc2aeb3fc49d3dc0ba54a7fa8ec251014b91814c
Scott McLeod <halcyonblue@gmail.com>
2014-02-06 19:26:49 -0500

    Updating to call InitializeReporting in Global.asax

--- Dashboards.aspx.cs
+++
@@ -1,5 +0,0 @@
-public partial class Dashboards : System.Web.UI.Page {
-  protected override void OnPreInit(System.EventArgs e) {
-    ASP.global_asax.CustomAdHocConfig.InitializeReporting();
-  }
-}
--- Dashboards.aspx.vb
+++ Dashboards.aspx.vb
@@ -1,4 +1,4 @@
-Partial Class DashboardViewer
+Partial Class Dashboards
   Inherits System.Web.UI.Page
   Protected Overrides Sub OnPreInit(ByVal e As System.EventArgs)
     ASP.global_asax.CustomAdHocConfig.InitializeReporting()
