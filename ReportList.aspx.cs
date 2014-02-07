261588a00897f38dbcb7f76d42ff8dd662d30bcb
Scott McLeod <halcyonblue@gmail.com>
2014-02-06 20:18:56 -0500

    Remove BOM from ReportList.aspx.cs

--- ReportList.aspx.cs
+++ ReportList.aspx.cs
@@ -1,4 +1,4 @@
-﻿public partial class ReportList : System.Web.UI.Page {
+public partial class ReportList : System.Web.UI.Page {
   protected override void OnPreInit(System.EventArgs e) {
     ASP.global_asax.CustomAdHocConfig.InitializeReporting();
   }
