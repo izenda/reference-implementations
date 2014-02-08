0abbcfee2e1f762ddd6a211efa5041c22e416f5a
Scott McLeod <halcyonblue@gmail.com>
2014-02-07 23:13:08 -0500

    Add windows find

--- ri-build.sh
+++ ri-build.sh
@@ -1,11 +1,40 @@
 #!/bin/bash
 RI=${1}
+
+case `uname -s` in
+  Darwin)
+    BUILD_TOOL=xbuild;
+    FIND="/usr/bin/find";
+    SED="gsed";
+    ;;
+  Linux)
+    BUILD_TOOL=xbuild;
+    FIND="/usr/bin/find";
+    SED="sed";
+    ;;
+  CYGWIN_NT*)
+    BUILD_TOOL=msbuild.exe;
+    FIND="/bin/find";
+    SED="sed";
+    ;;
+esac
+
+exitOnFailure() {
+  if [ "$?" -ne "0" ]
+  then
+    exit -1
+  fi
+}
+
 wget http://archives.izenda.us/core/latest/izenda.adhoc.zip
+exitOnFailure
 unzip izenda.adhoc.zip
 rm izenda.adhoc.zip
 "/cygdrive/c/Program Files (x86)/MSBuild/12.0/Bin/amd64/MSBuild.exe" ri.csproj
+exitOnFailure
 mkdir -p Reports
 touch Izenda.config
 mkdir -p "${RI}"
-find . \( -path "./.git" -o -path "./${RI}" \) -prune -o -print | cpio -mpvd "${RI}"
+${FIND} . \( -path "./.git" -o -path "./${RI}" \) -prune -o -print | cpio -mpvd "${RI}"
 zip -r ${RI}.zip ${RI}
+exitOnFailure
