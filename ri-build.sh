e1fdde245d5db039e30588674f8281ddc803436c
Scott McLeod <halcyonblue@gmail.com>
2014-02-07 22:50:20 -0500

    Fix ri-build script to call correct csproj

--- ri-build.sh
+++ ri-build.sh
@@ -3,7 +3,7 @@ RI=${1}
 wget http://archives.izenda.us/core/latest/izenda.adhoc.zip
 unzip izenda.adhoc.zip
 rm izenda.adhoc.zip
-"/cygdrive/c/Program Files (x86)/MSBuild/12.0/Bin/amd64/MSBuild.exe" "${RI}.csproj"
+"/cygdrive/c/Program Files (x86)/MSBuild/12.0/Bin/amd64/MSBuild.exe" ri.csproj
 mkdir -p Reports
 touch Izenda.config
 mkdir -p "${RI}"
