9973ed52289229925473f174351d681d34eca857
joshuanjordan85 <joshua.jordan@izenda.com>
2014-09-26 16:53:03 -0400

    put the files back for deployment

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports=function(grunt) {
+  //Project configuration for client release
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    //Tasks//
+    cssmin: {
+      css: {
+        files: [{
+          expand: true,
+          cwd: 'Resources/css/',
+          src: ['*.css','!*.min.css'],
+          dest: 'Resources/css/',
+          ext: '.min.css'
+        }]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.registerTask('default', ['cssmin']);
+};
+
