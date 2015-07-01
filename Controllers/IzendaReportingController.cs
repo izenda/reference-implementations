f67bf9edf9a7a0dfcb83feba1eeda247958d73da
Sergey Tananov <tananovsi@gmail.com>
2015-07-01 16:19:22 +0500

    FB-20267: Enable input and request validation to prevent XSS attacks

--- Controllers/IzendaReportingController.cs
+++ Controllers/IzendaReportingController.cs
@@ -11,50 +11,61 @@ using System.Web.UI.WebControls;
 using System.Reflection;
 
 namespace MVC3SK.Controllers {
-  public class ReportingController : Controller {
-    [ValidateInput(false)]
-    public ActionResult ReportDesigner() {
-      return View();
-    }
+	public class ReportingController : Controller {
 
-    [ValidateInput(false)]
-    public ActionResult ReportList() {
-      if (HttpContext.Request != null && !String.IsNullOrEmpty(HttpContext.Request.RawUrl) && !HttpContext.Request.RawUrl.ToLower().Contains(AdHocSettings.ReportList.ToLower())) {
-        return RedirectToAction("ReportList", "Reporting");
-      }
-      return View();
-    }
+		private void ValidateRequest()
+		{
+			// Force standard request validation
+			Request.ValidateInput();
+			// Validate Query string manually
+			for (int i = 0; i < Request.QueryString.Count; i++)
+			{
+				string val = Request.QueryString[i];
+			}
+		}
 
-    [ValidateInput(false)]
-    public ActionResult Settings() {
-      return View();
-    }
+		public ActionResult ReportDesigner() {
+			ValidateRequest();
+			return View();
+		}
 
-    [ValidateInput(false)]
-    public ActionResult Dash()
-    {
-        return View();
-    }
+		public ActionResult ReportList() {
+			Request.ValidateInput();
+			if (HttpContext.Request != null && !String.IsNullOrEmpty(HttpContext.Request.RawUrl) && !HttpContext.Request.RawUrl.ToLower().Contains(AdHocSettings.ReportList.ToLower())) {
+				return RedirectToAction("ReportList", "Reporting");
+			}
+			return View();
+		}
 
-    [ValidateInput(false)]
-    public ActionResult Dashboards() {
-      return View();
-    }
+		public ActionResult Settings() {
+			ValidateRequest();
+			return View();
+		}
 
-    [ValidateInput(false)]
-    public ActionResult ReportViewer() {
-      AdHocSettings.ShowSimpleModeViewer = true;
-      return View();
-    }
+		public ActionResult Dash(){
+			ValidateRequest();
+			return View();
+		}
 
-    [ValidateInput(false)]
-    public ActionResult InstantReport() {
-      return View();
-    }
+		public ActionResult Dashboards() {
+			ValidateRequest();
+			return View();
+		}
 
-    [ValidateInput(false)]
-    public ActionResult DashboardDesigner() {
-      return View();
-    }
-  }
+		public ActionResult ReportViewer() {
+			ValidateRequest();
+			AdHocSettings.ShowSimpleModeViewer = true;
+			return View();
+		}
+
+		public ActionResult InstantReport() {
+			ValidateRequest();
+			return View();
+		}
+
+		public ActionResult DashboardDesigner() {
+			ValidateRequest();
+			return View();
+		}
+	}
 }
