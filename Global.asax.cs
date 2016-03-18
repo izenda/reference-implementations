1e5eada88eff7d29bf78f2d229b1c171d8b1d9c0
Alex <alexey_pam@mail.ru>
2016-03-18 16:45:33 +0500

    FB-23694: Fixed newtonsoftjson-related exception

--- Global.asax.cs
+++ Global.asax.cs
@@ -85,7 +85,6 @@ namespace MVC4Razor2 {
     protected void Application_Start() {
       RouteTable.Routes.MapPageRoute("rs.aspx", "{*aspx}", "~/Reporting/rs.aspx", false, null, new RouteValueDictionary { { "aspx", new SpecificFileRouterConstraint("aspx", "rs.aspx") } });
       AreaRegistration.RegisterAllAreas();
-      WebApiConfig.Register(GlobalConfiguration.Configuration);
       FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
       RegisterRoutes(RouteTable.Routes);
       BundleConfig.RegisterBundles(BundleTable.Bundles);
