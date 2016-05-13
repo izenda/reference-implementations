653e0e3d0ad62324e1a2f13611163ea957d1a4d2
Sergey Tananov <tananovsi@gmail.com>
2016-05-13 11:28:11 +0500

    FB-24356: Respect AdHocSettings.RequireLogin setting on all Izenda pages

--- DashboardViewer.aspx.vb
+++ DashboardViewer.aspx.vb
@@ -2,5 +2,6 @@ Partial Class DashboardViewer
   Inherits System.Web.UI.Page
   Protected Overrides Sub OnPreInit(ByVal e As System.EventArgs)
     ASP.global_asax.CustomAdHocConfig.InitializeReporting()
+    Izenda.AdHoc.Utility.CheckUserName()
   End Sub
 End Class
--- InstantReportNew.aspx.vb
+++ InstantReportNew.aspx.vb
@@ -3,5 +3,6 @@ Partial Class InstantReportNew
     Inherits System.Web.UI.Page
     Protected Overrides Sub OnPreInit(ByVal e As System.EventArgs)
         ASP.global_asax.CustomAdHocConfig.InitializeReporting()
+        Izenda.AdHoc.Utility.CheckUserName()
     End Sub
 End Class
