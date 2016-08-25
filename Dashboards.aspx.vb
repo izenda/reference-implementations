985ec3abc93ec888dbb4515d4ca14c90869cbdf7
Sergey Tananov <tananovsi@gmail.com>
2016-08-25 12:44:46 +0500

    FB-25452: Typo in the Dashboards page code fixed

--- Dashboards.aspx.vb
+++ Dashboards.aspx.vb
@@ -2,6 +2,6 @@ Partial Class Dashboards
   Inherits System.Web.UI.Page
   Protected Overrides Sub OnPreInit(ByVal e As System.EventArgs)
     ASP.global_asax.CustomAdHocConfig.InitializeReporting()
-    Izenda.AdHoc.Utility.CheckUserName(
+    Izenda.AdHoc.Utility.CheckUserName()
   End Sub
 End Class
