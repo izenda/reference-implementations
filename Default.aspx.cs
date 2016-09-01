b4d3d865825c95366838a788f7c4c053144efc58
Alex <alexey_pam@mail.ru>
2016-09-01 12:32:37 +0600

    FB-25384: Made multi-user per browser working out of the box

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,5 +1,5 @@
 public partial class _Default : System.Web.UI.Page {
   protected override void OnLoad(System.EventArgs e) {
-    Response.Redirect("ReportList.aspx");
+    Response.Redirect(Izenda.AdHoc.Utility.AppendIzPidParameter("ReportList.aspx"));
   }
 }
