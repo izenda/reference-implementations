4825a1c45f8b9cea4bd4e4ab9a7b368a1d5ee3d5
Alex Yarmolchuk <alex.yarmolchuk@izenda.com>
2017-01-30 15:46:59 +0500

    FB-26743: Removed node_modules folder.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,33 @@
-module.exports=function(grunt) {
-  //Project configuration for client release
-
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-
-    //Tasks//
-    cssmin: {
-      css: {
-        files: [{
-          expand: true,
-          cwd: 'Resources/css/',
-          src: ['*.css','!*.min.css'],
-          dest: 'Resources/css/',
-          ext: '.min.css'
-        },
-        
-        {
-          expand: true,
-          cwd:    'Resources/css/ModernStyles/',
-          src:    ['*.css', '!*.min.css'],
-          dest:   'Resources/css/ModernStyles/',
-          ext:    '.min.css'
-        }]
-      }
-    }
-  });
-
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.registerTask('default', ['cssmin']);
+/**
+ * Required packages (in "package.json" file):
+ *"devDependencies": {
+ *  "grunt": "^1.0.1",
+ *  "grunt-contrib-cssmin": "^1.0.2"
+ *}
+ */
+module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		//Tasks
+		cssmin: {
+			css: {
+				files: [{
+					expand: true,
+					cwd: 'Resources/css/',
+					src: ['*.css', '!*.min.css'],
+					dest: 'Resources/css/',
+					ext: '.min.css'
+				}, {
+					expand: true,
+					cwd: 'Resources/css/ModernStyles/',
+					src: ['*.css', '!*.min.css'],
+					dest: 'Resources/css/ModernStyles/',
+					ext: '.min.css'
+				}]
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.registerTask('default', ['cssmin']);
 };
 
