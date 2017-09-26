83d9ccf4daf82839cfce4436843aafdc9a1cacf3
Alex <alexey_pam@mail.ru>
2017-09-26 17:19:07 +0600

    FB-28400: Made it opening reportList by default

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -10,7 +10,7 @@ namespace MVC4Razor2 {
     public static void RegisterRoutes(RouteCollection routes) {
       routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
       routes.MapRoute("IzendaReporting", "{controller}/{action}/{id}", new { controller = "Reporting", id = UrlParameter.Optional });
-      routes.MapRoute("Default", "{controller}/{action}/{id}", new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+      routes.MapRoute("Default", "{controller}/{action}/{id}", new { controller = "Reporting", action = "ReportList", id = UrlParameter.Optional });
     }
   }
 }
