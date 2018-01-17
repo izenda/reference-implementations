6d8e41f71ea488a435b0275f7c4859ced298cc21
Alex <alexey_pam@mail.ru>
2018-01-17 16:05:44 +0500

    FB-28673: Corrections according to code review

--- Izenda.master.vb
+++ Izenda.master.vb
@@ -5,15 +5,6 @@ Partial Class MasterPage1
 	Inherits MasterPage
 	Protected Overrides Sub OnInit(ByVal e As System.EventArgs)
 		ASP.global_asax.CustomAdHocConfig.InitializeReporting()
-		Dim imgUrl As String
-		If (AdHocSettings.ApplicationHeaderImageUrl = Nothing) Then
-			imgUrl = "ModernImages.IzendaNewLogoBlue.png"
-		Else
-			imgUrl = AdHocSettings.ApplicationHeaderImageUrl
-		End If
-		rightLogo.Src = String.Format("./{0}image={1}",
-			StaticResourceToolkit.ResourcesProviderUrl,
-			imgUrl)
 		If (AdHocSettings.ShowDesignLinks = False) Then
 			Dim script As String
 			script = "<script type=""text/javascript"" language=""javascript"">"
